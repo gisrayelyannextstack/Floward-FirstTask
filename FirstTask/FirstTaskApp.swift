c9493b8f9165219b9374912b0fd0751382b5f34d
gisrayelyannextstack <gerasimisrayelyan@nextstack.org>
2022-11-13 12:00:34 +0400

    Initial Commit

---
+++ FirstTask/FirstTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstTaskApp.swift
+//  FirstTask
+//
+//  Created by Gerasim Israyelyan on 13.11.22.
+//
+
+import SwiftUI
+
+@main
+struct FirstTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
